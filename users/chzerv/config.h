e607bed520677f37a3ab922755249a8f922dce31
Chris Zervakis <xrist.zerv@protonmail.com>
2025-04-10 15:42:22 +0300

    Increase TAPPING_TERM from 175 to 200

--- users/chzerv/config.h
+++ users/chzerv/config.h
@@ -1,7 +1,7 @@
 #pragma once
 
 // Home row mods
-#define TAPPING_TERM 175
+#define TAPPING_TERM 200
 #define TAPPING_TERM_PER_KEY
 #define PERMISSIVE_HOLD_PER_KEY
 #define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
